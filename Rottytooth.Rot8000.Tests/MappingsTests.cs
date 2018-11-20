d0858302186fa86f691f599718c395ffa6c1b33c
Daniel Temkin <d@danieltemkin.com>
2018-11-20 17:05:47 -0500

    Put all mappings into a dictionary for faster rotating -- fixes #2

--- Rottytooth.Rot8000.Tests/MappingsTests.cs
+++ Rottytooth.Rot8000.Tests/MappingsTests.cs
@@ -15,5 +15,24 @@ namespace Rottytooth.Rot8000.Tests
         {
             Assert.IsTrue(Rotator.Mappings.Count % 2 == 0);
         }
+
+        /// <summary>
+        /// Does it match what's previous live?
+        /// </summary>
+        [TestMethod]
+        public void MatchesPreviousIteration()
+        {
+            Assert.AreEqual(
+                Rotator.Rotate(@"安全靶点百度安全联姻TP-LINK 安全技术受热捧百度杀毒用户数破2000万360再度恶意拦截百度走进武汉大学鼓励大学生互联网安全创新2013全民双十一抢货，谁来为电脑加速？百度杀毒：双十一网购需警惕“吸血鬼”木马百度拦截DNS劫持木马，双重手段保网民安全"),
+                "흟촾ᮐ穀흟촾ѭ헑籝籙簶籕籒籗籔 흟촾쾭穀ﰊ簻簹簹簹짝簼簿簹쵣穀ᆉᏴ퓽휼⌼캇퓽휼쩨ѭͪ흟촾췱簻簹簺簼촾쾢켗짖ᅀ笶ယ쨐喇Ԫ칶ᐸ等穀筄쾢켗짖ͪᅆᬚཿ鯷퀎ౙὖ鯸Ẇ穀籍籗籜캁Ẇ笶쾢ᗦ쮳ͪ흟촾");
+        }
+
+        [TestMethod]
+        public void MatchesPreviousIterationLatin()
+        {
+            Assert.AreEqual(
+                Rotator.Rotate(@"HNEre is asdioja doij !~12213123123 12312 3123123"),
+                "籑籗籎类籮 籲籼 籪籼籭籲籸米籪 籭籸籲米 簪粇簺簻簻簺簼簺簻簼簺簻簼 簺簻簼簺簻 簼簺簻簼簺簻簼");
+        }
     }
 }
--- Rottytooth.Rot8000/Rotator.cs
+++ Rottytooth.Rot8000/Rotator.cs
@@ -10,7 +10,7 @@ namespace Rottytooth.Rot8000
     {
         public const int BMP_SIZE = 0x10000; // 65536
 
-        public static List<char> Mappings { get; set; }
+        public static Dictionary<char, char> Mappings { get; set; }
 
         private static int RotateNum { get; set; }
 
@@ -18,19 +18,27 @@ namespace Rottytooth.Rot8000
         {
             // We will populate this with all the non-control, non-surrogate characters.
             // When we rotate, we rotate through this list, which points to the valid characters (not control characters that get lost)
-            Mappings = new List<char>();
+            Mappings = new Dictionary<char, char>();
 
-            for (int i = char.MinValue; i <= BMP_SIZE; i++)
+            List<char> validList = new List<char>();
+
+            for (int i = char.MinValue; i < BMP_SIZE; i++)
             {
                 char c = (char)i;
 
                 if (!char.IsControl(c) && !char.IsWhiteSpace(c) && !char.IsLowSurrogate(c) && !char.IsHighSurrogate(c))
                 {
-                    Mappings.Add(c);
+                    validList.Add(c);
                 }
             }
 
-            RotateNum = Mappings.Count / 2;
+            RotateNum = validList.Count / 2;
+
+            for(int i = 0; i < validList.Count; i++)
+            {
+                int outnum = (i + RotateNum) % validList.Count;
+                Mappings[validList[i]] = validList[outnum];
+            }
         }
 
         public static string Rotate(string toConvert)
@@ -39,20 +47,13 @@ namespace Rottytooth.Rot8000
 
             for (int count = 0; count < toConvert.Length; count++)
             {
-                int charloc = Mappings.FindIndex(x => x == toConvert[count]);
-
-                // if it's out of range or a control character, don't rotate it, but add to the string
-                if (charloc < 0)
+                if (!Mappings.ContainsKey(toConvert[count]))
                 {
                     outputString.Append(toConvert[count]);
                     continue;
                 }
 
-                charloc += RotateNum;
-                charloc %= Mappings.Count;
-
-                outputString.Append(Mappings[charloc]);
-
+                outputString.Append(Mappings[toConvert[count]]);
             }
 
             return outputString.ToString();
